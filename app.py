ae6b72fff44a8f7119e85b034da2101d44ec1780
Samina <samina@Saminas-MacBook-Air.local>
2022-05-15 13:51:59 +0530

    my first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+# Program to Upload Color Image and convert into Black & White image
+import os
+from flask import  Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+# Open and redirect to default upload webpage
+@app.route('/')
+def load_form():
+    return render_template('upload.html')
+
+
+# Function to upload image and redirect to new webpage
+@app.route('/gray', methods=['POST'])
+def upload_image():
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join('static/', filename))
+
+    display_message = 'Image successfully uploaded and displayed below'
+    return render_template('upload.html', filename=filename, message = display_message)
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename=filename))
+
+
+if __name__ == "__main__":
+    app.run()
+
+
